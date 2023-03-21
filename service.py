4bc7895612321de1cdc204ea6798eccb6d66cb12
Souheil Chelfouh <trollfot@gmail.com>
2023-03-21 15:05:33 +0100

    Using asyncio/ZMQ solution

--- service.py
+++ service.py
@@ -1,11 +1,17 @@
-import pika
-import dynaconf
+import asyncio
+import time
+import email
 import sys
 import logging
+import dynaconf
+import typing as t
+from pathlib import Path
+from minicli import cli, run
 from mailbox import Maildir
-from postrider.mailer import SMTPConfiguration, Courrier
-from postrider.queue import ProcessorThread
 from postrider import create_message
+from postrider.queue import ProcessorThread
+from postrider.mailer import SMTPConfiguration, Courrier
+from aiozmq import rpc
 
 
 def configure_logging(log_level=logging.WARNING):
@@ -18,35 +24,45 @@ def configure_logging(log_level=logging.WARNING):
     )
 
 
-def create_connection():
-    credentials = pika.PlainCredentials("guest", "guest")
-    parameters = pika.ConnectionParameters(
-        "localhost", credentials=credentials
-    )
-    connection = pika.BlockingConnection(parameters)
-    return connection
+class CourrierService(rpc.AttrHandler):
+
+    def __init__(self, workers):
+        self.workers = workers
+
+    @rpc.method
+    def send_email(self,
+                   key: str,
+                   recipients: t.Iterable[str],
+                   subject: str,
+                   text: str,
+                   html: t.Optional[str] = None):
+
+        if key in self.workers:
+            try:
+                worker, config = self.workers[key]
+                message = create_message(
+                    config.emitter,
+                    recipients,
+                    subject,
+                    text,
+                    html
+                ).as_string()
+
+                msg = email.message_from_string(message)
+                worker.mailbox.add(msg)
+                return True
+            except:
+                return {"err": "Email corrupted"}
+        else:
+            return {"err": "unknown mailer"}
 
 
-def emailer_service(workers):
-    connection = create_connection()
-    try:
-        channel = connection.channel()
-        generator = channel.consume("mailing")
-        for method_frame, properties, body in generator:
-            worker, config = workers[method_frame.routing_key]
-            message = create_message(
-                config.emitter, ['test@example.com'], 'subject', body.decode())
-            worker.mailbox.add(message)
-            channel.basic_ack(method_frame.delivery_tag)
-    finally:
-        if connection.is_open:
-            connection.close()
-
-
-if __name__ == '__main__':
-    settings = dynaconf.Dynaconf(settings_files=["emailer.toml"])
+@cli
+async def serve(config: Path, debug: bool = True):
+    settings = dynaconf.Dynaconf(settings_files=[config])
     courrier = Courrier(SMTPConfiguration(**settings.smtp))
-    configure_logging()
+    level = logging.DEBUG if debug else logging.WARNING
+    configure_logging(log_level=level)
 
     workers = {}
     for emailer, config in settings['mailbox'].items():
@@ -55,13 +71,20 @@ if __name__ == '__main__':
             Maildir(config.mailbox),
             5.0  # salvo every 5 sec
         )
-        workers[f'mailing.{config.key}'] = (thread, config)
+        workers[config.key] = (thread, config)
 
     for worker, config in workers.values():
         worker.start()
     try:
-        emailer_service(workers)
+        service = CourrierService(workers)
+        server = await rpc.serve_rpc(
+            service, bind='tcp://127.0.0.1:5557')
+        await server.wait_closed()
     finally:
         for worker, config in workers.values():
             worker.stop()
             worker.join()
+
+
+if __name__ == '__main__':
+    run()
